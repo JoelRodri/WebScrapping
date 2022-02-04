eb3b79a621d5bf9c392c7467ec7a7ac4142a9cc0
jrodri <jrodri@elpuig.xeill.net>
2022-02-04 13:07:34 +0100

    v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -26,25 +27,69 @@ public class Main {
     String title = driver.getTitle();
     System.out.println(title);
 
-    List<Pilotos> piloto = new ArrayList<>();
     List<String> enlaces = new ArrayList<>();
 
 
     List<WebElement> buscaUrl = new ArrayList<>();
     buscaUrl = driver.findElements(new By.ByClassName("listing-item--link"));
+
     for (WebElement url : buscaUrl ){
       enlaces.add(url.getAttribute("href"));
       System.out.println(url.getAttribute("href"));
     }
 
+
+    WebElement nombre, numero;
+    int contador = 0;
     for (String enlace : enlaces){
       driver.navigate().to(enlace);
-      List<WebElement> nombre = buscaUrl = driver.findElements(new By.ByClassName("driver-name"));
-      List<WebElement> numero = buscaUrl = driver.findElements(new By.ByClassName("driver-number"));
-      List<WebElement> datos = buscaUrl = driver.findElements(new By.ByClassName("listing-item--link"));
+      nombre = driver.findElement(new By.ByClassName("driver-name"));
+      numero = driver.findElement(new By.ByClassName("driver-number"));
+      List<WebElement> datos = driver.findElements(new By.ByClassName("listing-item--link"));
 
-      nombre.add()
+//      nombre.add()
+      Pilotos piloto = new Pilotos();
+      piloto.setNombre(nombre.getText());
+      piloto.setNumero(numero.getText());
 
+      System.out.println(nombre.getText() + " " + numero.getText());
+
+      for (WebElement dato : datos){
+        switch (contador){
+          case 0:
+            piloto.setEquipo(dato.getText());
+            break;
+          case 1:
+            piloto.setPais(dato.getText());
+            break;
+          case 2:
+            piloto.setPodiums(dato.getText());
+            break;
+          case 3:
+            piloto.setPuntos(dato.getText());
+            break;
+          case 4:
+            piloto.setGpComp(dato.getText());
+            break;
+          case 5:
+            piloto.setTitulos(dato.getText());
+            break;
+          case 6:
+            piloto.setMejorPos(dato.getText());
+            break;
+          case 7:
+            piloto.setMejorClas(dato.getText());
+            break;
+          case 8:
+            piloto.setNacimiento(dato.getText());
+            break;
+          case 9:
+            piloto.setNacionalidad(dato.getText());
+            break;
+        }
+        System.out.println(dato.getText());
+        contador++;
+      }
     }
 
 
--- src/main/java/Pilotos.java
+++ src/main/java/Pilotos.java
@@ -14,18 +14,99 @@ public class Pilotos {
     String nacimiento;
     String nacionalidad;
 
-    public Pilotos(String nombre, String numero, String equipo, String pais, String podiums, String puntos, String gpComp, String titulos, String mejorPos, String mejorClas, String nacimiento, String nacionalidad) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void setNumero(String numero) {
         this.numero = numero;
+    }
+
+    public void setEquipo(String equipo) {
         this.equipo = equipo;
+    }
+
+    public void setPais(String pais) {
         this.pais = pais;
+    }
+
+    public void setPodiums(String podiums) {
         this.podiums = podiums;
+    }
+
+    public void setPuntos(String puntos) {
         this.puntos = puntos;
+    }
+
+    public void setGpComp(String gpComp) {
         this.gpComp = gpComp;
+    }
+
+    public void setTitulos(String titulos) {
         this.titulos = titulos;
+    }
+
+    public void setMejorPos(String mejorPos) {
         this.mejorPos = mejorPos;
+    }
+
+    public void setMejorClas(String mejorClas) {
         this.mejorClas = mejorClas;
+    }
+
+    public void setNacimiento(String nacimiento) {
         this.nacimiento = nacimiento;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
         this.nacionalidad = nacionalidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getPodiums() {
+        return podiums;
+    }
+
+    public String getPuntos() {
+        return puntos;
+    }
+
+    public String getGpComp() {
+        return gpComp;
+    }
+
+    public String getTitulos() {
+        return titulos;
+    }
+
+    public String getMejorPos() {
+        return mejorPos;
+    }
+
+    public String getMejorClas() {
+        return mejorClas;
+    }
+
+    public String getNacimiento() {
+        return nacimiento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
     }
 }
